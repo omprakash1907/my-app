f7ee489c53bf124f705e40b340e45398e064ef10
Kashyap297 <kashyap29700@gmail.com>
2025-05-12 02:33:07 +0530

    feat: reintroduce FeaturesPanel import in App component; adjust layout for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import FAQ from "./components/FAQ";
+import FeaturesPanel from "./components/FeaturesPanel";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
@@ -8,6 +9,7 @@ import LearnMore from "./components/LearnMore";
 import OutputValues from "./components/OutputValues";
 import PricingSection from "./components/PricingSection";
 
+
 function App() {
   return (
     <>
@@ -15,10 +17,6 @@ function App() {
         <Header />
         <main className="flex-1">
           <HeroSection />
-<<<<<<< HEAD
-          {/* <FeaturesPanel /> */}
-=======
->>>>>>> e7a0bd7b9107f3c5972dc1ab482f56443995d4b8
           <KeyBenefits />
           <PricingSection />
           <OutputValues />
